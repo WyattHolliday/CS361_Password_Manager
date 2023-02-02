bafb32685998e3bc0684e69beb7c69ded3acfc9a
Wyatt Holliday <hollidaw@flip2.engr.oregonstate.edu>
2023-02-01 22:30:42 -0800

    Added sidebar html and css + acc edit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function toggleAccountSlideOut(event) {
     var Slideout = document.getElementById("account-slideout")
     if (Slideout.classList.contains("hidden")) {
         Slideout.classList.remove("hidden")
+        closeOtherSlides("account-slideout")
     } else {
         Slideout.classList.add("hidden")
     }
@@ -11,6 +12,7 @@ function toggleCreateSlideOut(event) {
     var Slideout = document.getElementById("create-slideout")
     if (Slideout.classList.contains("hidden")) {
         Slideout.classList.remove("hidden")
+        closeOtherSlides("create-slideout")
     } else {
         Slideout.classList.add("hidden")
     }
@@ -20,6 +22,7 @@ function toggleSortSlideOut(event) {
     var Slideout = document.getElementById("sort-slideout")
     if (Slideout.classList.contains("hidden")) {
         Slideout.classList.remove("hidden")
+        closeOtherSlides("sort-slideout")
     } else {
         Slideout.classList.add("hidden")
     }
@@ -29,6 +32,7 @@ function toggleCustomizeSlideOut(event) {
     var Slideout = document.getElementById("customize-slideout")
     if (Slideout.classList.contains("hidden")) {
         Slideout.classList.remove("hidden")
+        closeOtherSlides("customize-slideout")
     } else {
         Slideout.classList.add("hidden")
     }
@@ -38,6 +42,7 @@ function toggleSettingsSlideOut(event) {
     var Slideout = document.getElementById("settings-slideout")
     if (Slideout.classList.contains("hidden")) {
         Slideout.classList.remove("hidden")
+        closeOtherSlides("settings-slideout")
     } else {
         Slideout.classList.add("hidden")
     }
@@ -47,9 +52,28 @@ function toggleSupportSlideOut(event) {
     var Slideout = document.getElementById("support-slideout")
     if (Slideout.classList.contains("hidden")) {
         Slideout.classList.remove("hidden")
+        closeOtherSlides("support-slideout")
     } else {
         Slideout.classList.add("hidden")
     }
+}
+
+function closeOtherSlides(currSlide) {
+    var Slideouts = document.getElementsByClassName("slideout-page")
+    for (var i = 0; i < Slideouts.length; i++) {
+        if(Slideouts[i].id != currSlide) {
+            Slideouts[i].classList.add("hidden")
+        }
+    }
+}
+
+function closeAccountEdit() {
+    accountEditContainer = document.getElementsByClassName("account-edit-container")[0]
+    accountEditContainer.classList.add("hidden")
+    document.getElementById("edit-url").value = "www.website.com"
+    document.getElementById("edit-user-username").value = "Username"
+    document.getElementById("edit-user-password").value = "Password"
+    document.getElementById("edit-user-email").value = "email@gmail.com"
 }
 
 window.onload = function() {
@@ -68,13 +92,30 @@ window.onload = function() {
     var settingsSideButton = document.getElementById("settings-item")
     settingsSideButton.addEventListener("click", toggleSettingsSlideOut)
 
-    var supportSideButton = document.getElementById("support-item")
-    supportSideButton.addEventListener("click", toggleSupportSlideOut)
-
+    // var supportSideButton = document.getElementById("support-item")
+    // supportSideButton.addEventListener("click", function() {
+    //     window.open("https://www.passwordmanager/support", "_blank")
+    // })
     
     var clearSearchButton = document.getElementById("clear-search-button")
     clearSearchButton.addEventListener("click", function() {
         searchBar = document.getElementsByClassName("search-bar")[0]
         searchBar.value = ""
     })
+
+    var editClose = document.getElementsByClassName("edit-close")[0]
+    editClose.addEventListener("click", closeAccountEdit)
+    var revertButton = document.getElementById("edit-revert")
+    revertButton.addEventListener("click", closeAccountEdit)
+
+    var accInstEditButton = document.getElementById("acc-inst-edit-button")
+    accInstEditButton.addEventListener("click", function() {
+        var accountEditContainer = document.getElementsByClassName("account-edit-container")[0]
+        accountEditContainer.classList.remove("hidden")
+    })
+
+    document.getElementById("edit-url").value = "www.website.com"
+    document.getElementById("edit-user-username").value = "Username"
+    document.getElementById("edit-user-password").value = "Password"
+    document.getElementById("edit-user-email").value = "email@gmail.com"
 }
